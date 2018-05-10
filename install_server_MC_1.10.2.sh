daced6cd25f6e365e69a30d4587fc54f3d29434d
Thomas Bnt <14293805+thomasbnt@users.noreply.github.com>
2018-05-10 13:46:53 +0200

    1.10.2 to 1.12.2

--- install_server_MC_1.10.2.sh
+++
@@ -1,6 +0,0 @@
-wget https://s3.amazonaws.com/Minecraft.Download/versions/1.10.2/minecraft_server.1.10.2.jar
-echo "eula=true" > eula.txt
-echo "#!/bin/bash
-java -Xmx2048M -Xms2048M -jar minecraft_server.1.10.2.jar nogui" > start.sh
-echo ">> Start MC server"
-sh start.sh
